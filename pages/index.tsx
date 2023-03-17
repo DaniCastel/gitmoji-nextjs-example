f5237cbac8e6878eb8f2bdb134d619b13074adfc
Daniela Castellanos <ldcastellanosa@gmail.com>
2023-03-17 09:39:25 -0500

    :zap: Improve query performance

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,7 @@ export default function Home() {
       <h1>Feature 1</h1>
       {/* <div>:construction: Work in progress</div> */}
       {/* <div>:art: Improve structure / format of the code</div>  */}
-      {/* <div>:zap: Improve query performance</div> */}
+      <div>:zap: Improve query performance</div>
       <div>:sparkles: Feature 2</div>
       {/* <div>:sparkles: New plugin</div>  */}
       {/* <div>:lipstick: PLD UI improvements</div>  */}
